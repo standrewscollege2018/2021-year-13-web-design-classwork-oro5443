3da1ead31b94bc95bcd4660093602b1deebdf296
oro5443 <60494659+oro5443@users.noreply.github.com>
2021-03-19 15:03:56 +1300

    19/03/2021

--- studentwebsite/deletestudent.php
+++ studentwebsite/deletestudent.php
@@ -1,6 +1,6 @@
 <?php
   $studentID = $_GET['studentID'];
-
+  echo "$studentID";
   $deletestudent_sql = "SELECT * FROM student WHERE studentID=$studentID";
   $deletestudent_qry = mysqli_query($dbconnect, $deletestudent_sql);
   $deletestudent_aa = mysqli_fetch_assoc($deletestudent_qry);
@@ -13,25 +13,7 @@
           <div class='jumbotron'>
             <img src='images/$photo' class='img-fluid' alt=''>
             </br>$firstname $lastname
-            <button onclick='myFunction()'>DELETE?</button>
           </div>
         </div>
       ";
  ?>
-
- <script>
- function myFunction() {
-   var txt;
-   var r = confirm("Are You Sure You Want To Delete <?php echo "$firstname" ?>?");
-   if (r == true) {
-     alert("Student Has Been Deleted");
-     <?php
-      echo "yesz";
-      ?>
-     location.replace("index.php?page=displaydelete");
-   } else if (r == false) {
-     alert("Deletion Cancelled");
-     location.replace("index.php?page=displaydelete");
-   }
- }
- </script>
--- studentwebsite/displaydelete.php
+++ studentwebsite/displaydelete.php
@@ -18,17 +18,28 @@
         $lastname = $result_aa['lastname'];
         $photo = $result_aa['photo'];
         $studentID = $result_aa['studentID'];
-        
+
         echo "<div class='col-lg-4 col-md-4 bg-danger'>
-          <a href='index.php?page=deletestudent&studentID=$studentID'><img src='images/$photo' class='img-fluid' alt=''></a>
+          <button onclick='myFunction($studentID)'><img src='images/$photo' class='img-fluid' alt=''></button>
           <p>$firstname $lastname</p>
         </div>";
 
         } while ($result_aa = mysqli_fetch_assoc($result_qry));
-
-
   }
 
  ?>
 </div>
 </div>
+
+<script>
+function myFunction($studentID) {
+  var r = confirm("Student Selected For Deletion: " +$studentID);
+  if (r == true) {
+    alert("Student Has Been Deleted");
+    location.replace('index.php?page=deletestudent&studentID='+$studentID)
+  } else if (r == false) {
+    alert("Deletion Cancelled");
+    location.replace('index.php?page=displaydelete');
+  }
+}
+</script>
